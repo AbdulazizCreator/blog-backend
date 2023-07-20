025b04f1e4a741c6f5542b73d6f91438c9d8bdf6
abdulazizcreator <abdulazizprogrammer@gmail.com>
2023-07-20 05:19:47 +0500

    vercel

---
+++ seeder.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Load models
+const Post = require("./models/Post");
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Post.deleteMany();
+    console.log("Data Destroyed...".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+deleteData();
